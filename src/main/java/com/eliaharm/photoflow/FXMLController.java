8f858d33ea56b3e1f867e93d3c81ec6bc62caaf6
Elia Harmouche <eliaharm@users.noreply.github.com>
2018-05-02 22:02:59 +0300

    all dep with pom error

--- src/main/java/com/eliaharm/photoflow/FXMLController.java
+++ src/main/java/com/eliaharm/photoflow/FXMLController.java
@@ -1,8 +1,13 @@
 package com.eliaharm.photoflow;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.eliaharm.photoflow.jpa.Imported_files;
 import com.eliaharm.photoflow.mtp.MTPFileManager;
 import com.eliaharm.photoflow.mtp.MTPUtil;
 import com.eliaharm.photoflow.util.ProgramDirectoryUtilities;
@@ -13,42 +18,59 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import jmtp.PortableDevice;
 
-
-
+import io.ebean.Ebean;
+import io.ebean.EbeanServer;
 
 public class FXMLController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-        try {
-        	System.out.println(ProgramDirectoryUtilities.getProgramDirectory());
-        	System.out.println(System.getProperty("user.dir"));
-            System.out.println(this.getClass().getProtectionDomain().getCodeSource().getLocation());
-			MTPUtil.addLibraryPath(System.getProperty("user.dir")+"\\target\\classes\\");
+
+	final static Logger logger = LoggerFactory.getLogger(MainApp.class);
+
+	private static EbeanServer server = Ebean.getServer(null);
+
+	@FXML
+	private Label label;
+
+	@FXML
+	private void handleButtonAction(ActionEvent event) {
+		logger.info("Entering application: handleButtonAction");
+		System.out.println("You clicked me!");
+		label.setText("Hello World!");
+		try {
+			System.out.println(ProgramDirectoryUtilities.getProgramDirectory());
+			System.out.println(System.getProperty("user.dir"));
+			System.out.println(this.getClass().getProtectionDomain().getCodeSource().getLocation());
+			MTPUtil.addLibraryPath(System.getProperty("user.dir") + "\\target\\classes\\");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        MTPUtil.printAll();
-        MTPFileManager jMTPmng = new MTPFileManager();
-        PortableDevice[] devices =jMTPmng.getDevices();
-        for (int i = 0; i < devices.length; i++){
-            System.out.println(devices[i].toString());
-        jMTPmng.openDevice(devices[i]);
-        }
-        jMTPmng.close();
-        
-        
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+		MTPUtil.printAll();
+		MTPFileManager jMTPmng = new MTPFileManager();
+		PortableDevice[] devices = jMTPmng.getDevices();
+		for (int i = 0; i < devices.length; i++) {
+			System.out.println(devices[i].toString());
+			jMTPmng.openDevice(devices[i]);
+		}
+		jMTPmng.close();
+
+	}
+
+	@FXML
+	private void handleButton2Action(ActionEvent event) {
+		logger.info("Entering application: handleButton2Action DB");
+
+		Imported_files Files = Ebean.find(Imported_files.class, 1);
+		logger.info(Files.getFile_name_path());
+		logger.info(Files.toString());
+
+		List<Imported_files> query = Ebean.find(Imported_files.class).findList();
+		logger.info(query.iterator().toString());
+
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}
 }
---
+++ src/main/java/com/eliaharm/photoflow/jpa/Imported_files.java
@@ -0,0 +1,53 @@
+package com.eliaharm.photoflow.jpa;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+//import lombok.ToString;
+
+/**
+ * @author elia_harmouche
+ */
+//@ToString
+@Entity
+@Table(name = "tbl_imported_files")
+public class Imported_files {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Basic
+    private String job_id;
+
+    @Basic
+    private String file_name_path;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getJob_id() {
+        return this.job_id;
+    }
+
+    public void setJob_id(String job_id) {
+        this.job_id = job_id;
+    }
+
+    public String getFile_name_path() {
+        return this.file_name_path;
+    }
+
+    public void setFile_name_path(String file_name_path) {
+        this.file_name_path = file_name_path;
+    }
+
+}
